bd993e1e1e5895bc67c448bd37d689819b637ca7
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2019-08-23 14:35:16 +0600

    calls passed to handler for receiving message

--- src/GenericConsumableMessage.php
+++ src/GenericConsumableMessage.php
@@ -2,17 +2,18 @@
 
 namespace Anik\Amqp;
 
+use Closure;
+
 class GenericConsumableMessage extends ConsumableMessage
 {
     private $handler;
 
-    public function __construct ($handler) {
+    public function __construct (Closure $handler) {
         parent::__construct();
         $this->handler = $handler;
     }
 
     public function handle () {
-        echo $this->getStream() . PHP_EOL;
-        $this->getDeliveryInfo()->acknowledge();
+        $this->handler->call($this, $this);
     }
 }
