f1c2a92bd08d55f64ab729fc140631a451579d50
Alex Wijnholds <1023654+Alexwijn@users.noreply.github.com>
2019-10-17 14:15:26 +0200

    BindingKey and RoutingKey is not required

--- src/helpers.php
+++ src/helpers.php
@@ -1,14 +1,14 @@
 <?php
 
 if (!function_exists('consume')) {
-    function consume ($closure, $bindingKey, array $config = [])
+    function consume ($closure, $bindingKey = '', array $config = [])
     {
         resolve('amqp')->consume($closure, $bindingKey, $config);
     }
 }
 
 if (!function_exists('publish')) {
-    function publish ($message, $routingKey, array $config = [])
+    function publish ($message, $routingKey = '', array $config = [])
     {
         resolve('amqp')->publish($message, $routingKey, $config);
     }
