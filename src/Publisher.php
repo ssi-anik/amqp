bb8941d996fac21dc63352fa5944adaf41412ad9
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-10-10 11:01:09 +0600

    publisher class added

--- src/Publisher.php
+++ src/Publisher.php
@@ -2,74 +2,60 @@
 
 namespace Anik\Amqp;
 
-use Anik\Amqp\Exceptions\AmqpException;
-use PhpAmqpLib\Channel\AMQPChannel;
-use PhpAmqpLib\Message\AMQPMessage;
-use PhpAmqpLib\Wire\AMQPTable;
+use Anik\Amqp\Exchanges\Exchange;
+use Anik\Amqp\Messages\Publish\Message;
 
 class Publisher
 {
-    /* @var \PhpAmqpLib\Channel\AMQPChannel */
-    private $channel;
-
-    /**
-     * @return \PhpAmqpLib\Channel\AMQPChannel|null
-     */
-    public function getChannel () : ?AMQPChannel {
-        return $this->channel;
+    public function publish(
+        Connection $connection,
+        Exchange $exchange,
+        Message $message,
+        string $routingKey = '',
+        array $options = []
+    ): bool {
+        return $this->publishBulk($connection, $exchange, [$message], $routingKey, $options);
     }
 
-    /**
-     * @param \PhpAmqpLib\Channel\AMQPChannel $channel
-     *
-     * @return self
-     */
-    public function setChannel ($channel) : self {
-        $this->channel = $channel;
-
-        return $this;
-    }
-
-    /**
-     * @param \Anik\Amqp\PublishableMessage $message
-     * @param                               $routingKey
-     *
-     * @throws \Anik\Amqp\Exceptions\AmqpException
-     */
-    public function publish (PublishableMessage $message, $routingKey) {
-        $this->publishBulk([ $message ], $routingKey);
-    }
-
-    /**
-     * @param \Anik\Amqp\PublishableMessage[] $messages
-     * @param                                 $routingKey
-     *
-     * @throws \Anik\Amqp\Exceptions\AmqpException
-     */
-    public function publishBulk ($messages, $routingKey) {
-        if (!$this->channel instanceof AMQPChannel) {
-            throw new AmqpException('Channel is not defined.');
-        }
+    public function publishBulk(
+        Connection $connection,
+        Exchange $exchange,
+        array $messages,
+        string $routingKey = '',
+        array $options = []
+    ): bool {
+        $channelId = $options['channel_id'] ?? null;
+        $channel = $connection->getChannel($channelId);
 
         if (count($messages) === 0) {
-            throw new AmqpException('No message to pass to exchange.');
+            return false;
         }
 
-        $ep = $messages[0]->getExchange()->getProperties();
-
-        $channel = $this->getChannel();
-
-        if (isset($ep['declare']) && $ep['declare']) {
-            $channel->exchange_declare($ep['name'], $ep['type'], $ep['passive'] ?? false, $ep['durable'] ?? true, $ep['auto_delete'] ?? false, $ep['internal'] ?? false, $ep['nowait'] ?? false, new AMQPTable($ep['properties'] ?? []));
+        if ($exchange->shouldDeclare()) {
+            $channel->exchange_declare(
+                $exchange->getName(),
+                $exchange->getType(),
+                $exchange->getPassive(),
+                $exchange->isDurable(),
+                $exchange->isAutoDelete(),
+                $exchange->isInternal(),
+                $exchange->isNoWait(),
+                $exchange->getArguments(),
+                $exchange->getTicket()
+            );
         }
 
         $max = 200;
-        foreach ( $messages as $message ) {
-            $channel->batch_basic_publish(new AMQPMessage($message->getStream(), $message->getProperties()), $ep['name'], $routingKey);
+
+        /** @var Message $message */
+        foreach ($messages as $message) {
+            $channel->batch_basic_publish($message->prepare(), $exchange->getName(), $routingKey);
 
             --$max <= 0 ? $max = 200 && $channel->publish_batch() : null;
         }
 
         $channel->publish_batch();
+
+        return true;
     }
 }
