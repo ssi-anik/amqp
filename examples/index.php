345a9fd3d7f718c5ec9bba277dbf8bf1d96121ae
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-10-10 11:01:24 +0600

    examples added

---
+++ examples/index.php
@@ -0,0 +1,21 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use Anik\Amqp\Connection;
+use Anik\Amqp\Exchanges\Fanout;
+use Anik\Amqp\Messages\Publish\Message;
+use Anik\Amqp\Publisher;
+
+$host = '127.0.0.1';
+$port = 5672;
+$username = 'user';
+$password = 'bitnami';
+
+$connection = new Connection($host, $port, $username, $password);
+// $exchange = Exchange::make(['name' => 'example.direct', 'type' => 'direct', 'declare' => true]);
+$exchange = (new Fanout('example.fanout'))->setDeclare(true);
+$message = new Message('my message');
+
+$status = (new Publisher())->publish($connection, $exchange, $message);
+var_dump($status);
