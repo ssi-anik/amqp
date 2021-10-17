3fd0ea4d83bb0de8eb505f41f4ea382bdf186b8c
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-10-17 23:10:40 +0600

    queue test added

---
+++ tests/Unit/Queue/QueueTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Anik\Amqp\Tests\Unit\Queue;
+
+use Anik\Amqp\Exceptions\AmqpException;
+use Anik\Amqp\Exchanges\Exchange;
+use PHPUnit\Framework\TestCase;
+
+class QueueTest extends TestCase
+{
+    public function testPositive() {
+        $this->assertTrue(true);
+    }
+}
