79f587a9cfe93b89416db30b1ae40c60d48f2e7c
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-10-23 21:56:34 +0600

    amqp connection factory added

---
+++ src/AmqpConnectionFactory.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Anik\Amqp;
+
+use Anik\Amqp\Exceptions\AmqpException;
+use PhpAmqpLib\Connection\AbstractConnection;
+use PhpAmqpLib\Connection\AMQPLazySSLConnection;
+
+class AmqpConnectionFactory
+{
+    public static function make(
+        string $host,
+        int $port,
+        string $user,
+        ?string $password,
+        string $vhost = '/',
+        array $options = [],
+        ?string $amqpConnectionClass = null
+    ): AbstractConnection {
+        $hosts[] = [
+            'host' => $host,
+            'port' => $port,
+            'user' => $user,
+            'password' => $password,
+            'vhost' => $vhost,
+        ];
+
+        return static::makeFromArray($hosts, $options, $amqpConnectionClass);
+    }
+
+    public static function makeFromArray(
+        array $hosts,
+        array $options = [],
+        ?string $amqpConnectionClass = null
+    ): AbstractConnection {
+        $amqpConnectionClass = $amqpConnectionClass ?? AMQPLazySSLConnection::class;
+        if (!is_subclass_of($amqpConnectionClass, AbstractConnection::class)) {
+            throw new AmqpException('$amqpConnectionClass must be a class name of AbstractConnection::class');
+        }
+
+        return $amqpConnectionClass::create_connection($hosts, $options);
+    }
+}
