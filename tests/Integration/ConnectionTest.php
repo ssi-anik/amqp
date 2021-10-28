9ec9fcd9e8bd43197da5a062c2a19fef2f414592
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-10-28 15:19:28 +0600

    connection testcases update for exchange make or reconfigure

--- tests/Integration/ConnectionTest.php
+++ tests/Integration/ConnectionTest.php
@@ -2,7 +2,12 @@
 
 namespace Anik\Amqp\Tests\Integration;
 
+use Anik\Amqp\Exceptions\AmqpException;
+use Anik\Amqp\Exchanges\Direct;
+use Anik\Amqp\Exchanges\Exchange;
+use Anik\Amqp\Exchanges\Fanout;
 use Anik\Amqp\Producer;
+use Closure;
 use Exception;
 
 class ConnectionTest extends AmqpTestCase
@@ -139,5 +144,94 @@ class ConnectionTest extends AmqpTestCase
 
         $producer = new Producer($this->connection);
         $this->assertSame($this->channel, $producer->getChannelWithId(5));
+    }
+
+    /**
+     * @dataProvider exchangeConfigureDataProvider
+     *
+     * @param array $data
+     */
+    public function testConnectionIsAbleToMakeOrConfigureExchange(array $data)
+    {
+        $exchange = $data['exchange'] ?? null;
+        $options = $data['options'] ?? [];
+        $checks = $data['checks'] ?? [];
+        $producer = new Producer($this->connection);
+
+        $configuredExchange = Closure::fromCallable(
+            function () use ($exchange, $options) {
+                return $this->makeOrReconfigureExchange($exchange, $options);
+            }
+        )->call($producer);
+
+        foreach ($checks as $method => $expectation) {
+            $this->assertEquals($configuredExchange->$method(), $expectation);
+        }
+    }
+
+    public function testNameAndTypeIsRequiredWhenConnectionMakesExchangeFromOptions()
+    {
+        $this->expectException(AmqpException::class);
+        $producer = new Producer($this->connection);
+
+        Closure::fromCallable(
+            function () {
+                return $this->makeOrReconfigureExchange(null, []);
+            }
+        )->bindTo($producer)->call($producer);
+    }
+
+    public function exchangeConfigureDataProvider(): array
+    {
+        return [
+            'should create an exchange' => [
+                [
+                    'exchange' => null,
+                    'options' => [
+                        'name' => self::EXCHANGE_NAME,
+                        'type' => Exchange::TYPE_DIRECT,
+                    ],
+                    'checks' => [
+                        'shouldDeclare' => false,
+                        'isPassive' => false,
+                        'isAutoDelete' => false,
+                        'isDurable' => true,
+                    ],
+                ],
+            ],
+            'should reconfigure exchange if provided to the method' => [
+                [
+                    'exchange' => new Fanout(self::EXCHANGE_NAME),
+                    'options' => [
+                        'durable' => false,
+                        'auto_delete' => true,
+                        'internal' => true,
+                        'arguments' => ['key' => 'value'],
+                        'ticket' => 20,
+                    ],
+                    'checks' => [
+                        'getName' => self::EXCHANGE_NAME,
+                        'isAutoDelete' => true,
+                        'isInternal' => true,
+                        'isDurable' => false,
+                        'getArguments' => ['key' => 'value'],
+                        'getTicket' => 20,
+                    ],
+                ],
+            ],
+            'exchange is not reconfigured if options are not provided' => [
+                [
+                    'exchange' => new Direct(self::EXCHANGE_NAME),
+                    'checks' => [
+                        'getName' => self::EXCHANGE_NAME,
+                        'isAutoDelete' => false,
+                        'isInternal' => false,
+                        'isDurable' => true,
+                        'getArguments' => [],
+                        'getTicket' => null,
+                    ],
+                ],
+            ],
+        ];
     }
 }
