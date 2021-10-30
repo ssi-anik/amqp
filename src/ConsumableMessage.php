f9f3fc4b1abff6adce446cacd90eefa54b71101a
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-10-30 21:09:53 +0600

    generic consumable class functionality update

--- src/ConsumableMessage.php
+++ src/ConsumableMessage.php
@@ -2,6 +2,7 @@
 
 namespace Anik\Amqp;
 
+use Anik\Amqp\Exceptions\AmqpException;
 use PhpAmqpLib\Message\AMQPMessage;
 
 class ConsumableMessage implements Consumable
@@ -16,9 +17,51 @@ class ConsumableMessage implements Consumable
         $this->callable = $callable;
     }
 
+    public function ack(bool $multiple = false): bool
+    {
+        if (!$this->message) {
+            return false;
+        }
+
+        $this->message->ack($multiple);
+
+        return true;
+    }
+
+    public function nack(bool $requeue = false, bool $multiple = false): bool
+    {
+        if (!$this->message) {
+            return false;
+        }
+
+        $this->message->nack($requeue, $multiple);
+
+        return true;
+    }
+
+    public function reject(bool $requeue = true): bool
+    {
+        if (!$this->message) {
+            return false;
+        }
+
+        $this->message->reject($requeue);
+
+        return true;
+    }
+
+    public function getMessageBody(): string
+    {
+        if (!$this->message) {
+            throw new AmqpException('Message is not received yet.');
+        }
+
+        return $this->message->getBody();
+    }
+
     public function handle(): void
     {
-        call_user_func($this->callable, $this->message);
+        call_user_func($this->callable, $this, $this->message);
     }
 
     public function setMessage(AMQPMessage $message): Consumable
