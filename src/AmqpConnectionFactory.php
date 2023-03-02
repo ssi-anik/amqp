a74fca08994814889aae1fa5ef2edc63e14b4c27
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2023-03-02 14:17:27 +0600

    comply with php-amqplib:^3.2.x to avoid warnings (#27)
    * docker ports wrapped around quotes
    
    * uses AmqpConnectionConfig to build connection
    
    * workflow updated to support PHP 8.{1,2}

--- src/AmqpConnectionFactory.php
+++ src/AmqpConnectionFactory.php
@@ -4,10 +4,25 @@ namespace Anik\Amqp;
 
 use Anik\Amqp\Exceptions\AmqpException;
 use PhpAmqpLib\Connection\AbstractConnection;
-use PhpAmqpLib\Connection\AMQPLazySSLConnection;
+use PhpAmqpLib\Connection\AMQPConnectionConfig;
+use PhpAmqpLib\Connection\AMQPConnectionFactory as PhpAmqplibConnectionFactory;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use Throwable;
 
 class AmqpConnectionFactory
 {
+    protected static $builder = null;
+
+    public static function builder(ConfigBuilder $builder)
+    {
+        static::$builder = $builder;
+    }
+
+    public static function getBuilder(): ConfigBuilder
+    {
+        return static::$builder ?? new AmqpConnectionConfigBuilder();
+    }
+
     public static function make(
         string $host,
         int $port,
@@ -33,11 +48,28 @@ class AmqpConnectionFactory
         array $options = [],
         ?string $amqpConnectionClass = null
     ): AbstractConnection {
-        $amqpConnectionClass = $amqpConnectionClass ?? AMQPLazySSLConnection::class;
-        if (!is_subclass_of($amqpConnectionClass, AbstractConnection::class)) {
-            throw new AmqpException('$amqpConnectionClass expects a classname that extends AbstractConnection::class');
+        if ($amqpConnectionClass && !isset($options['io'])) {
+            $options['io'] = is_a($amqpConnectionClass, AMQPStreamConnection::class, true)
+                ? AMQPConnectionConfig::IO_TYPE_STREAM
+                : AMQPConnectionConfig::IO_TYPE_SOCKET;
+            $options['lazy'] = strpos($amqpConnectionClass, 'Lazy') !== false;
         }
 
-        return $amqpConnectionClass::create_connection($hosts, $options);
+        if (empty($hosts)) {
+            throw new AmqpException('Hosts cannot be empty.');
+        }
+
+        $lastException = null;
+        $builder = static::getBuilder();
+
+        foreach ($hosts as $host) {
+            try {
+                return PhpAmqplibConnectionFactory::create($builder->build($host + $options));
+            } catch (Throwable $t) {
+                $lastException = $t;
+            }
+        }
+
+        throw new AmqpException($lastException->getMessage());
     }
 }
--- tests/Unit/AmqpConnectionFactoryTest.php
+++ tests/Unit/AmqpConnectionFactoryTest.php
@@ -3,17 +3,21 @@
 namespace Anik\Amqp\Tests\Unit;
 
 use Anik\Amqp\AmqpConnectionFactory;
+use Anik\Amqp\ConfigBuilder;
 use Anik\Amqp\Exceptions\AmqpException;
-use Anik\Amqp\Producer;
+use PhpAmqpLib\Connection\AbstractConnection;
+use PhpAmqpLib\Connection\AMQPConnectionConfig;
+use PhpAmqpLib\Connection\AMQPLazyConnection;
 use PhpAmqpLib\Connection\AMQPLazySocketConnection;
-use PhpAmqpLib\Connection\AMQPLazySSLConnection;
+use PhpAmqpLib\Connection\AMQPSocketConnection;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PHPUnit\Framework\TestCase;
 
 class AmqpConnectionFactoryTest extends TestCase
 {
-    public function loginCredentials(): array
+    public function makeMethodDataProvider(): array
     {
-        return [
+        $parameters = [
             'host' => '127.0.0.1',
             'port' => 5672,
             'user' => 'user',
@@ -21,55 +25,146 @@ class AmqpConnectionFactoryTest extends TestCase
             'vhost' => '/',
             'options' => [],
         ];
+
+        return [
+            'socket connection is returned if option does not specify io type' => [
+                $parameters,
+                AMQPSocketConnection::class,
+            ],
+            'checks io type from options array to make connection' => [
+                array_merge_recursive($parameters, ['options' => ['io' => 'stream', 'lazy' => true]]),
+                AMQPStreamConnection::class,
+            ],
+            'considers class name from method param to build io type' => [
+                array_merge($parameters, ['classname' => AMQPLazySocketConnection::class]),
+                AMQPSocketConnection::class,
+            ],
+            'throws exception if cannot make a connection' => [
+                // Will fail for sure, because no active RabbitMQ service running and is not lazy
+                array_merge($parameters, ['classname' => AMQPStreamConnection::class]),
+                null,
+                AmqpException::class,
+            ],
+        ];
     }
 
-    public function testMakeMethodReturnsAmqpLazySslConnectionInstanceByDefault()
+    public function makeFromArrayMethodDataProvider(): array
     {
-        $instance = AmqpConnectionFactory::make(...array_values($this->loginCredentials()));
-        $this->assertInstanceOf(AMQPLazySSLConnection::class, $instance);
+        $parameters = [
+            'hosts' => [
+                [
+                    'host' => '127.0.0.1',
+                    'port' => 5672,
+                    'user' => 'user',
+                    'password' => 'password',
+                    'vhost' => '/',
+                    'options' => [],
+                ],
+            ],
+            'options' => [],
+        ];
+
+        return [
+            'socket connection is returned if option does not specify io type' => [
+                $parameters,
+                AMQPSocketConnection::class,
+            ],
+            'checks io type from options array to make connection' => [
+                array_merge_recursive($parameters, ['options' => ['io' => 'stream', 'lazy' => true]]),
+                AMQPStreamConnection::class,
+            ],
+            'considers class name from method param to build io type' => [
+                array_merge($parameters, ['classname' => AMQPLazyConnection::class]),
+                AMQPStreamConnection::class,
+            ],
+            'throws exception if cannot make a connection' => [
+                // Will fail for sure, because no active RabbitMQ service running and is not lazy
+                array_merge($parameters, ['classname' => AMQPStreamConnection::class]),
+                null,
+                AmqpException::class,
+            ],
+            'throws exception when hosts array is empty' => [
+                ['hosts' => [], 'options' => ['io' => 'socket', 'lazy' => true]],
+                null,
+                ['class' => AmqpException::class, 'message' => 'Hosts cannot be empty.'],
+            ],
+        ];
     }
 
-    public function testMakeMethodCanCreateAnyAmqpConnection()
+    /** @dataProvider makeMethodDataProvider */
+    public function testMakeMethod($parameters, $expected = null, $exception = null)
     {
-        $instance = AmqpConnectionFactory::make(
-            ...array_values(
-                   array_merge($this->loginCredentials(), ['amqpConnectionClass' => AMQPLazySocketConnection::class])
-               )
+        if ($exception) {
+            $this->expectException(AmqpException::class);
+        }
+
+        $connection = AmqpConnectionFactory::make(
+            $parameters['host'],
+            $parameters['port'],
+            $parameters['user'],
+            $parameters['password'],
+            $parameters['vhost'],
+            $parameters['options'],
+            $parameters['classname'] ?? null
         );
-        $this->assertInstanceOf(AMQPLazySocketConnection::class, $instance);
+
+        if ($expected) {
+            $this->assertInstanceOf($expected, $connection);
+        }
     }
 
-    public function testMakeMethodAcceptsSubClassOfAbstractConnectionToCreateInstance()
+    /** @dataProvider makeFromArrayMethodDataProvider */
+    public function testMakeFromArrayMethod($parameters, $expected = null, $exception = null)
     {
-        $this->expectException(AmqpException::class);
-        AmqpConnectionFactory::make(
-            ...array_values(
-                   array_merge($this->loginCredentials(), ['amqpConnectionClass' => Producer::class])
-               )
+        if ($exception) {
+            $this->expectException($exception['class'] ?? $exception);
+
+            if ($exception['message'] ?? null) {
+                $this->expectExceptionMessage($exception['message']);
+            }
+        }
+
+        $connection = AmqpConnectionFactory::makeFromArray(
+            $parameters['hosts'],
+            $parameters['options'],
+            $parameters['classname'] ?? null
         );
+
+        if ($expected) {
+            $this->assertInstanceOf($expected, $connection);
+        }
     }
 
-    public function testMakeFromArrayMethodReturnsAmqpLazySslConnectionInstanceByDefault()
+    public function testFactoryUsesCustomBuilderIfProvided()
     {
-        $hosts = [$this->loginCredentials()];
+        $configBuilder = $this->getMockBuilder(ConfigBuilder::class)
+                              ->disableOriginalConstructor()
+                              ->getMock();
 
-        $instance = AmqpConnectionFactory::makeFromArray($hosts, []);
-        $this->assertInstanceOf(AMQPLazySSLConnection::class, $instance);
-    }
+        $configBuilder->expects($this->exactly(2))->method('build')->willReturnCallback(function() {
+            $config = new AMQPConnectionConfig();
+            // sets lazy connection so that doesn't try to connect to the service
+            $config->setIsLazy(true);
+            return $config;
+        });
 
-    public function testMakeFromArrayMethodCanCreateAnyAmqpConnection()
-    {
-        $hosts = [$this->loginCredentials()];
+        AmqpConnectionFactory::builder($configBuilder);
+        $host = [
+            'host' => '127.0.0.1',
+            'port' => 5672,
+            'user' => 'user',
+            'password' => 'password',
+            'vhost' => '/',
+        ];
 
-        $instance = AmqpConnectionFactory::makeFromArray($hosts, [], AMQPLazySocketConnection::class);
-        $this->assertInstanceOf(AMQPLazySocketConnection::class, $instance);
-    }
+        $this->assertInstanceOf(
+            AbstractConnection::class,
+            AmqpConnectionFactory::make($host['host'], $host['port'], $host['user'], $host['password'], $host['vhost'])
+        );
 
-    public function testMakeFromArrayMethodAcceptsSubClassOfAbstractConnectionToCreateInstance()
-    {
-        $hosts = [$this->loginCredentials()];
-
-        $this->expectException(AmqpException::class);
-        AmqpConnectionFactory::makeFromArray($hosts, [], Producer::class);
+        $this->assertInstanceOf(
+            AbstractConnection::class,
+            AmqpConnectionFactory::makeFromArray([$host])
+        );
     }
 }
